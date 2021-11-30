694a03f9bf9152b8ac78d0ddb90075db2c8b645a
ictus <ictus@ictusui-MacBookPro.local>
2021-11-30 09:48:20 +0900

    Fin.

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,8 +1,9 @@
-import React, {useEffect} from 'react';
-import styles from './navgation.module.css';
+import React, {useEffect, useState} from 'react';
+import styles from './navigation.module.css';
 import {useNavigate} from "react-router-dom";
 
 const Navigation = ({authService,user}) => {
+    const [navHeight, setNavHeight] = useState('');
     const navigate = useNavigate()
     const goHome = () => {
         navigate("/")
@@ -11,8 +12,9 @@ const Navigation = ({authService,user}) => {
         authService.logout()
         window.location.reload();
     }
+
     return(
-        <nav className={styles.nav}>
+        <nav id='navbar' className={styles.nav}>
             { window.location.pathname === "/" ||
             <span className={styles.home} onClick={goHome}>Back</span>
             }
--- client/src/components/Selected/Selected.js
+++ client/src/components/Selected/Selected.js
@@ -3,6 +3,7 @@ import Axios from "axios";
 import HTMLReactParser from "html-react-parser";
 import TextEditor from "../TextEditor/TextEditor";
 import { useNavigate } from "react-router-dom";
+import timeFormatter from "../../util/date";
 import styles from './selected.module.css';
 
 const Selected = ({selected, isOwner, setBanner, setIsAlert}) => {
@@ -42,25 +43,6 @@ const Selected = ({selected, isOwner, setBanner, setIsAlert}) => {
             setIsAlert(true)
         }
     }
-    // Time Formatter
-    const timeFormatter = (createdAt) => {
-        const milliSeconds = new Date() - new Date(createdAt);
-        const seconds = milliSeconds / 1000;
-        if (seconds < 60) return `방금 전`;
-        const minutes = seconds / 60;
-        if (minutes < 60) return `${Math.floor(minutes)}분 전`;
-        const hours = minutes / 60;
-        if (hours < 24) return `${Math.floor(hours)}시간 전`;
-        const days = hours / 24;
-        if (days < 2) return `${Math.floor(days)}일 전`;
-        if (days >= 2) {
-            const dateObj = new Date(createdAt);
-            const year = dateObj.getFullYear();
-            const month = dateObj.getMonth() + 1;
-            const date = dateObj.getDate();
-            return `${year}년 ${month}월 ${date}일`;
-        }
-    };
     return(
         <main>
             {/*Section*/}
